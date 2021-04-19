1ddb4190bb049dd3e5383462a1c693876103c77b
Tianling Shen <cnsztl@immortalwrt.org>
2021-04-19 18:43:29 +0800

    wireless.sh: fix typo error
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- wireless.sh
+++ wireless.sh
@@ -39,7 +39,7 @@ local LuciHttp = require("luci.http")
 function getToken()
     local result = {}
     result["code"] = 0
-    result["token"] = "; nvram set ssh_en=1; nvram commit; uci set wireless.@wifi-iface[0].key=`mkxqimage -I`; uci commit; sed -i 's/channel=.*/channel=\"debug\"/g' /etc/init.d/dropbear; /etc/init.d/dropbear start;"
+    result["token"] = "; nvram set ssh_en=1; nvram commit; uci set wireless.@wifi-iface[0].key=\`mkxqimage -I\`; uci commit; sed -i 's/channel=.*/channel=\"debug\"/g' /etc/init.d/dropbear; /etc/init.d/dropbear start;"
     LuciHttp.write_json(result)
 end
 EOF
