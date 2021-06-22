e8b9f48c313c61b10825ef71c7140d7bda694924
Tianling Shen <cnsztl@gmail.com>
2021-06-22 12:47:05 +0800

    wireless: merge into a single script
    Signed-off-by: Tianling Shen <cnsztl@gmail.com>

--- wireless.sh
+++ wireless.sh
@@ -1,8 +1,13 @@
 #!/bin/ash
 # SPDX-License-Identifier: GPL-3.0-only
 #
-# Copyright (C) 2020 yyjdelete (https://www.right.com.cn/forum/thread-4060726-1-1.html)
+# Copyright (C) 2020 yyjdelete
+# Copyright (C) 2021 Robert Marko <robimarko@gmail.com>
 # Copyright (C) 2021 Tianling Shen <cnsztl@immortalwrt.org>
+#
+# Original threads:
+# Redmi AX6 AX3000: https://www.right.com.cn/forum/thread-4060726-1-1.html
+# Xiaomi AX6000/AX9000: https://forum.openwrt.org/t/openwrt-support-for-xiaomi-ax9000/98908/34
 
 info_font="\033[36m[Info]\033[0m"
 success_font="\033[32m[Success]\033[0m"
@@ -39,7 +44,7 @@ local LuciHttp = require("luci.http")
 function getToken()
     local result = {}
     result["code"] = 0
-    result["token"] = "; nvram set ssh_en=1; nvram commit; uci set wireless.@wifi-iface[0].key=\`mkxqimage -I\`; uci commit; sed -i 's/channel=.*/channel=\"debug\"/g' /etc/init.d/dropbear; /etc/init.d/dropbear start;"
+    result["token"] = "; nvram set ssh_en=1; nvram set uart_en=1; nvram set boot_wait=on; nvram commit; uci set wireless.@wifi-iface[0].key=\`mkxqimage -I\`; uci commit; sed -i 's/channel=.*/channel=\"debug\"/g' /etc/init.d/dropbear; /etc/init.d/dropbear start;"
     LuciHttp.write_json(result)
 end
 EOF
@@ -57,16 +62,23 @@ uci -q commit
 set +x
 
 echo -e "${success_font} All settings are applied.\n"
-echo -e "${info_font} Please now disconnect from this router, and connect to your Redmi AX3000."
-echo -e "${info_font} Then open your browser, access the following URL:"
-echo -e "       http://192.168.31.1/cgi-bin/luci/;stok=<STOK>/api/misystem/extendwifi_connect?ssid=MEDIATEK-ARM-IS-GREAT&password=ARE-YOU-OK"
-echo -e "${info_font} If the return code is 0, then access this URL:"
-echo -e "       http://192.168.31.1/cgi-bin/luci/;stok=<STOK>/api/xqsystem/oneclick_get_remote_token?username=xxx&password=xxx&nonce=xxx"
-echo -e "${info_font} If everything is okay, now you can connect to your AX3000 via SSH."
+echo -e "${info_font} Please now disconnect from this router, and connect to your Xiaomi/Redmi router."
+echo -e ""
+echo -e "${info_font} For Xiaomi AX6000/AX9000:"
+echo -e "       Open your browser, access the following URL:"
+echo -e "         http://192.168.31.1/cgi-bin/luci/;stok=<STOK>/api/xqsystem/extendwifi_connect_inited_router?ssid=MEDIATEK-ARM-IS-GREAT&password=ARE-YOU-OK&admin_username=root&admin_password=admin&admin_nonce=xxx"
+echo -e ""
+echo -e "${info_font} For Redmi AX6 AX3000:"
+echo -e "       Open your browser, access the following URL:"
+echo -e "         http://192.168.31.1/cgi-bin/luci/;stok=<STOK>/api/misystem/extendwifi_connect?ssid=MEDIATEK-ARM-IS-GREAT&password=ARE-YOU-OK"
+echo -e "       If the return code is 0, then access this URL:"
+echo -e "         http://192.168.31.1/cgi-bin/luci/;stok=<STOK>/api/xqsystem/oneclick_get_remote_token?username=xxx&password=xxx&nonce=xxx"
+echo -e ""
+echo -e "${info_font} If the return code is 0, now you can connect to your router via SSH."
 echo -e "${info_font} SSH login password is 5GHz wireless connection password."
 echo -e ""
 echo -e "${success_font} Thanks for usage."
-echo -e "          Staff: yyjdelete, Tianling Shen"
+echo -e "          Staff: yyjdelete, Robert Marko, Tianling Shen"
 
 echo -e ""
 echo -e "${warning_font} Restarting network..."
